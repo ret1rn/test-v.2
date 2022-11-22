70c677c68561195094b2aa698989edacbb446ce5
k1zns <tomas777bloody@gmail.com>
2022-11-22 21:26:00 +0500

    text v.2

---
+++ js class/main.js
@@ -0,0 +1,6 @@
+let age = confirm("Число?")
+let name = prompt("What is your name?").toUpperCase()
+
+if (name[0] == "A" && age === true) {
+    console.log("wel");
+} else {console.log("go home");}
