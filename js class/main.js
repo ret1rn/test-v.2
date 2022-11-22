68bfa08a02d4ed8601a6f64878f98be8500f7c66
k1zns <tomas777bloody@gmail.com>
2022-11-22 22:13:04 +0500

    updated js code

--- js class/main.js
+++ js class/main.js
@@ -1,28 +1,28 @@
-// let a = prompt("Число?")
+let a = prompt("Число?")
 
-// if (a % 2 == 0) {
-//     console.log("Четное");
-// } else {
-//     console.log("Не четное");
-// }
+if (a % 2 == 0) {
+    console.log("Четное");
+} else {
+    console.log("Не четное");
+}
 
-// let name = prompt("What is your name?").toLowerCase().length
-// let age = prompt("How old are you?")
+let name = prompt("What is your name?").toLowerCase().length
+let age = prompt("How old are you?")
 
-// if (name % 2 == 0 && age >= 18) {
-//     console.log("Welcome");
-// } else console.log("Fuck you");
+if (name % 2 == 0 && age >= 18) {
+    console.log("Welcome");
+} else console.log("Fuck you");
 
 let quests = "Mirziyoyev Putin Obama Biden Zelenskiy Trump"
 
-let namePrezident = prompt("Name Prezinent?")
+let namePresident = prompt("Name President?")
 
-namePrezident = namePrezident[0].toUpperCase() + namePrezident.slice(1)
+namePresident = namePresident[0].toUpperCase() + namePresident.slice(1)
 
 quests = quests.split(" ")
 
-if (namePrezident === quests[0] || namePrezident === quests[1] || namePrezident === quests[2] || namePrezident === quests[3] || namePrezident === quests[4] || namePrezident === quests[5]) {
-    console.log("welcome");
+if (namePresident === quests[0] || namePresident === quests[1] || namePresident === quests[2] || namePresident === quests[3] || namePresident === quests[4] || namePresident === quests[5]) {
+    console.log("welcome to the conference");
 } else {
-    console.log("Bye bye");
+    console.log("Bye bye Mr.President");
 }
